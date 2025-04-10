571698a7bc247e4c7bd437134c9f72c3fe1c5abb
vicentejrmc <vicente_pc@outlook.com>
2025-04-10 17:33:55 -0300

    Implementada Classe Amigo + Metodo ValidaEntradas

---
+++ ClubeDaLeitura.ConsoleApp/Amigo.cs
@@ -0,0 +1,25 @@
+﻿namespace ClubeDaLeitura.ConsoleApp;
+
+public class Amigo
+{
+    public string Nome;
+    public string Responsavel;
+    public string Telefone;
+
+
+    public string ValidarEntradas()
+    { 
+        string resultadoValidacao = "";
+
+        if(string.IsNullOrWhiteSpace(Nome) || Nome.Length < 3 || Nome.Length > 100)
+            resultadoValidacao += "O campo nome não pode ser vazio, e deve ter entre 3 e 100 caracteres.\n";
+
+        if (string.IsNullOrWhiteSpace(Responsavel) || Responsavel.Length < 3 || Responsavel.Length > 100)
+            resultadoValidacao += "O campo Responsável não pode ser vazio, e deve ter entre 3 e 100 caracteres.\n";
+
+        if (string.IsNullOrWhiteSpace(Telefone) || Telefone.Length < 10 || Telefone.Length > 15)
+            resultadoValidacao += "O campo Telefone não pode ser vazio, e deve ter entre 10 e 15 caracteres.\n";
+
+        return resultadoValidacao;
+    }
+}
