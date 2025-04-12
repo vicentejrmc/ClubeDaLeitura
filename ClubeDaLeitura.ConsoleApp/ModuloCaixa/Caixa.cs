8c54797dec8c78f1437c3e7d4d7666d378ee3aaa
vicentejrmc <vicente_pc@outlook.com>
2025-04-12 09:55:47 -0300

    Implementação Inicial Classe Caixa

--- ClubeDaLeitura.ConsoleApp/ModuloCaixa/Caixa.cs
+++ ClubeDaLeitura.ConsoleApp/ModuloCaixa/Caixa.cs
@@ -1,6 +1,40 @@
-﻿namespace ClubeDaLeitura.ConsoleApp.ModuloCaixa
+﻿using System.Drawing;
+
+namespace ClubeDaLeitura.ConsoleApp.ModuloCaixa
 {
-    interna class Caixa
+    public class Caixa
     {
+        public string Etiqueta;
+        public Color Cor;
+        public int DiasEmprestimo;
+
+        public Caixa(string etiqueta, Color cor, int diasEmprestimo)
+        {
+            Etiqueta = etiqueta;
+            Cor = cor;
+            DiasEmprestimo = diasEmprestimo;
+        }
+
+        public Caixa(string etiqueta, Color cor) // Sobre Carga para DiasEmprestimo Padrão
+        {
+            Etiqueta = etiqueta;
+            Cor = cor;
+            DiasEmprestimo = 7;
+        }
+
+
+        public void ValidarCaixa()
+        {
+            string caixaEhvalida = "";
+
+            if (Etiqueta.Length > 50)
+                caixaEhvalida += "!...A Etiqueda deve conter um texto únido de no maximo 50 caracteres...!";
+
+            if (DiasEmprestimo == null)
+                caixaEhvalida += "Você Não Definio valor para o o limite de dias do Emprestimo...(Valor padrão = 7)!";
+
+        }
+        // Implementar Validaçoes para: Etiqueta Duplicada, Dias Emprestimo(caso esteja em brando/Usar ou Não, valor padrão)
+
     }
 }
