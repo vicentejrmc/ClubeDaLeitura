dd05febf48bab6b524c7c2555df10b78255e8643
vicentejrmc <vicente_pc@outlook.com>
2025-04-17 15:46:33 -0300

    Limpando a class Program

--- ClubeDaLeitura.ConsoleApp/Program.cs
+++ ClubeDaLeitura.ConsoleApp/Program.cs
@@ -6,51 +6,14 @@ using ClubeDaLeitura.ConsoleApp.ModuloRevistas;
 
 namespace ClubeDaLeitura.ConsoleApp
 {
-    internal class Program : TelaPrincipal
+    internal class Program
     {
         static void Main(string[] args)
         {
-            TelaPrincipal telaPrincipal = new TelaPrincipal();
-            TelaAmigo telaAmigo = new TelaAmigo();
-            TelaCaixa telaCaixa = new TelaCaixa();
-            TelaRevista telaRevistas = new TelaRevista();
-            TelaEmprestimo telaEmprestimo = new TelaEmprestimo();
+            
+ 
 
-            string opcao;
-
-            do
-            {
-                Console.Clear();
-                opcao = telaPrincipal.MenuPrincipal();
-
-                switch (opcao)
-                {
-                    case "1":
-                        telaAmigo.GerenciarAmigos();
-                        break;
-
-                    case "2":
-                        telaCaixa.GerenciarCaixas();
-                        break;
-
-                    case "3":
-                        telaRevistas.GerenciarRevistas();
-                        break;
-
-                    case "4":
-                        telaEmprestimo.GerenciarEmprestimo();
-                        break;
-
-                    case "S":
-                        Console.WriteLine("Saindo do sistema...");
-                        break;
-
-                    default:
-                        Console.WriteLine("Opção inválida! Tente novamente.");
-                        break;
-                }
-
-            } while (opcao != "S");
+           
             
             
         }
