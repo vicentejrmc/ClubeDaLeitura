d998d64fe42283de3b2ce14ed524e3fef38cf03a
vicentejrmc <vicente_pc@outlook.com>
2025-04-29 16:22:14 -0300

    Implementado Interface IRepositorio

---
+++ ClubeDaLeitura.ConsoleApp/Compatilhado/IRepositorio.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubeDaLeitura.ConsoleApp.Compatilhado
+{
+    public interface IRepositorio<T> where T : EntidadeBase<T>
+    {
+
+        public void CadastrarRegistro(T novoRegistro);
+
+        public bool EditarRegistro(int idRegistro, T registroEditado);
+
+        public bool ExcluirRegistro(int idRegistro);
+
+        public T SelecionarRegistroPorId(int idRegistro);
+
+        public List<T> SelecionarTodos();
+    }
+}
